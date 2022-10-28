f08590618410e49ee1a9b5fc1265e0030cf96ea8
Turria101 <94504141+Turria101@users.noreply.github.com>
2022-10-28 14:49:50 +0200

    Update user.routes.ts

--- src/routes/api/user.routes.ts
+++ src/routes/api/user.routes.ts
@@ -8,11 +8,11 @@ const usrRouts = Router();
 // add Routers assined to first Endpoint 
 usrRouts.route('/all').get( authenticationMiddleware,handlers.GetAllUsers);
 usrRouts.route( '/:id').get( authenticationMiddleware,handlers.GetSpecificUser);
-usrRouts.route('/register').post(authenticationMiddleware,handlers.CreateRecordUser)
+usrRouts.route('/register').post(handlers.CreateRecordUser)
 usrRouts.route('/:id/edit').patch( authenticationMiddleware,handlers.UpdateRecordUser);
 usrRouts.route('/:id/delete').delete( authenticationMiddleware,handlers.DeleteUser);
 
 // CREATE Auth
-usrRouts.route('/signin').post(handlers.authentication)
+usrRouts.route('/signin').post( authenticationMiddleware,handlers.authentication)
 
 export default usrRouts; 
